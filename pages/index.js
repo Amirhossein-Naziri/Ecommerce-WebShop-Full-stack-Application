04417c54d2720d3668bcc39fb8a14ae286b17c4d
amir <san800000@gmail.com>
2023-01-27 17:35:11 +0330

    connected to sanity client

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,25 @@ import React from 'react';
 import Product from '../components/Product';
 
 import HeroBanner from '../components/HeroBanner';
+import FooterBanner from '../components/FooterBanner';
 
 const index = () => {
   return (
     <>
       <HeroBanner/>
+
+        <div className='products-heading'>
+          <h2>Best Seller Products</h2>
+          <p>iphone 14 Apple</p>
+        </div>
+
+        <div className='products-container'>
+          {[
+            "product 1 " , "product 2"].map((product) => product)}
+        </div>
+
+      <FooterBanner/>
+    
     </>
   )
 }
