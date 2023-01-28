c6302b82d6ed1a9b065dff46fec2187263335565
amir <san800000@gmail.com>
2023-01-28 16:01:54 +0330

    implement heroBanner Component

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,41 @@
 import React from 'react';
 // import {Product , FooterBanner , HeroBanner } from "./../components/"
 import Product from '../components/Product';
-
 import HeroBanner from '../components/HeroBanner';
 import FooterBanner from '../components/FooterBanner';
+import {client} from "../lib/client"
 
-const index = () => {
+const index = ({products , bannerData}) => {
   return (
     <>
-      <HeroBanner/>
-
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
+        
         <div className='products-heading'>
           <h2>Best Seller Products</h2>
           <p>iphone 14 Apple</p>
         </div>
 
         <div className='products-container'>
-          {[
-            "product 1 " , "product 2"].map((product) => product)}
+          {products?.map((product) => product)}
         </div>
 
       <FooterBanner/>
     
     </>
   )
+
+}
+export const getServerSideProps = async()=>{
+    const productQuery = '*[_type == "product"]';
+    const product = await client.fetch(productQuery);
+
+    const bannerQuery = '*[_type == "banner"]';
+    const bannerData = await client.fetch(bannerQuery);
+
+    return {
+      props:{product , bannerData}
+    }
+    
 }
 
-export default index
+export default index;
